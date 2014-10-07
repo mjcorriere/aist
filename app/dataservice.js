39a79507bad6a924771d73107c88f2e77e8ec6cf
mjc <markjcorriere@gmail.com>
2014-10-07 19:11:49 -0400

    Preliminary selection working. Preparing to parse data / return promises to controllers

--- app/dataservice.js
+++ app/dataservice.js
@@ -1,4 +1,4 @@
-hs3.factory('DataService', [function() {
+hs3.factory('DataService', ['$http', function($http) {
   
   var stormList   = [];
   var ghList      = [];
@@ -6,8 +6,36 @@ hs3.factory('DataService', [function() {
   var DataService = {};
 
   DataService.getStormList = function() {
+
+    // var rawStorms = $http.get('data/hurricanes.dat');
+
     
-    stormList = [{
+    stormList = [{"name" : "Alberto", "selected" : false, "available" : true},
+      {"name" : "Beryl", "selected" : false, "available" : true},
+      {"name" : "Chris", "selected" : false, "available" : true},
+      {"name" : "Debby", "selected" : false, "available" : false},
+      {"name" : "Ernesto", "selected" : false, "available" : true},
+      {"name" : "Florence", "selected" : false, "available" : true},
+      {"name" : "Helene", "selected" : false, "available" : true},
+      {"name" : "Gordon", "selected" : false, "available" : false},
+      {"name" : "Isaac", "selected" : false, "available" : true},
+      {"name" : "Joyce", "selected" : false, "available" : true},
+      {"name" : "Kirk", "selected" : false, "available" : false},
+      {"name" : "Leslie", "selected" : false, "available" : false},
+      {"name" : "Michael", "selected" : false, "available" : true},
+      {"name" : "Nadine", "selected" : false, "available" : true},
+      {"name" : "Oscar", "selected" : false, "available" : true},
+      {"name" : "Patty", "selected" : false, "available" : true},
+      {"name" : "Rafael", "selected" : false, "available" : true},
+      {"name" : "Sandy", "selected" : false, "available" : true},
+      {"name" : "Tony", "selected" : false, "available" : true}
+    ];
+
+    return stormList;
+  }
+
+  DataService.getGhList    = function() {
+    ghList = [{
       "name"      : "November"
       , "flights" : [{
           "gh" : "AV-6"
@@ -22,13 +50,44 @@ hs3.factory('DataService', [function() {
           , "date" : "10/09"
           , "available" : false
       }]
-    }]
-
-    return stormList;
+    }]    
+    return ghList;
   }
 
-  DataService.getGhList    = function() {
-    return ghList;
+  DataService.getHurricaneData      = function() {
+
+    var request = $http.get('data/hurricanes.dat');
+
+    return request.then(parseHurricaneData, handleError);
+
+  }
+
+  function parseHurricaneData(response) {
+    console.log('parsing data');
+    var data = response.data;
+    var lines = data.split('\n');
+
+    for(var i = 0; i < lines.length; i++) {
+      var line = lines[i];
+      if((line.indexOf('**') < 0) && (line !== '')) {
+        var splitLine = line.split(',');
+        var name = splitLine[1].trim();
+        var numPoints = parseInt(splitLine[2].trim());
+        console.log(name, numPoints);
+
+        // SPLIT OUT THE REMAINING LINES. DATE/TIME
+        // RETURN
+
+
+      }
+    }
+
+    // REPLACE WITH ACTUAL PARSING OF ACTUAL DATA;
+    return [DataService.getStormList(), DataService.getGhList()];
+  }
+
+  function handleError(response) {
+    console.log('An unknown error occured:',response);
   }
 
   return DataService;
--- app/selectionctrl.js
+++ app/selectionctrl.js
@@ -1,7 +1,42 @@
 hs3.controller('SelectionCtrl', ['$scope', 'DataService', 
   function($scope, DataService) {
 
-  $scope.helloWorld = 'goodbye world';
-  $scope.stormList = DataService.getStormList();
+  $scope.stormList = [];
+  $scope.ghList    = [];
+
+  loadData();
+
+  // $scope.stormList = DataService.getStormList();
+  // $scope.ghList    = DataService.getGhList();
+
+  function loadData() {
+    DataService.getHurricaneData()
+      .then(function(data) {
+        $scope.stormList = data[0];
+        $scope.ghList    = data[1];
+      });
+  }
+
+  $scope.selectStorm = function(index) {
+    var storm = $scope.stormList[index];
+    if (storm.available) {
+      storm.selected = !storm.selected;
+      console.log(storm.name, 'selected: ', $scope.stormList[index].selected);
+      $scope.updateAvailability(storm.start, storm.end);
+    } else {
+      console.log(storm.name, 'unavailable for selection');
+    }
+  }
+
+  $scope.updateAvailability = function(start, end) {
+    for(var i = 0; i < $scope.stormList.length; i++) {
+
+    }
+
+    for(var i = 0; i < $scope.ghList.length; i++) {
+
+    }
+
+  }
 
 }]);
